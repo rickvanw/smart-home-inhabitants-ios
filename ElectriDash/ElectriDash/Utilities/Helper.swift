65615f1e0aadba08ff6ef22f40c104514377d8ec
Unknown <rickvanweersel@hotmail.com>
2018-04-04 15:56:24 +0200

    Init Login

---
+++ ElectriDash/ElectriDash/Utilities/Helper.swift
@@ -0,0 +1,15 @@
+//
+//  Helper.swift
+//  ElectriDash
+//
+//  Created by Rick van Weersel on 04/04/2018.
+//  Copyright © 2018 Rick van Weersel. All rights reserved.
+//
+
+import UIKit
+
+
+class Helper {
+    
+
+}
