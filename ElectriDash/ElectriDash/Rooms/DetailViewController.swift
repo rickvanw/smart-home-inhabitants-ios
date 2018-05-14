87ab85123c01cdba86f16ffe1d80621658acb22e
Ruben Assink <ruben_assink@hotmail.com>
2018-05-14 11:33:48 +0200

    Extension UIImageView added

--- ElectriDash/ElectriDash/Rooms/DetailViewController.swift
+++ ElectriDash/ElectriDash/Rooms/DetailViewController.swift
@@ -21,13 +21,11 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
     // MARK: - Navigation
 
--- ElectriDash/ElectriDash/Rooms/ViewController.swift
+++ ElectriDash/ElectriDash/Rooms/ViewController.swift
@@ -11,27 +11,6 @@ import UIKit
 import Alamofire
 
 // Extension to download an Image asynchronously
-extension UIImageView {
-    
-    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() {
-                self.image = image
-            }
-            }.resume()
-    }
-    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
-        guard let url = URL(string: link) else { return }
-        downloadedFrom(url: url, contentMode: mode)
-    }
-}
 
 struct Room: Decodable {
     let energyUsage: Double
