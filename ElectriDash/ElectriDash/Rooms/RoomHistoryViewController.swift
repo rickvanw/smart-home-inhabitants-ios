34a9ade0a8d749336f833c0ea7b944805a661cde
Ruben Assink <ruben_assink@hotmail.com>
2018-06-11 13:56:29 +0200

    History device tableview

--- ElectriDash/ElectriDash/Rooms/RoomHistoryViewController.swift
+++ ElectriDash/ElectriDash/Rooms/RoomHistoryViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 import ScrollableGraphView
 import Alamofire
 
-class RoomHistoryViewController: UIViewController, RoomPageControllerToPage, ScrollableGraphViewDataSource, PeriodSettingDelegate{
+class RoomHistoryViewController: UIViewController,UITableViewDataSource, UITableViewDelegate, RoomPageControllerToPage, ScrollableGraphViewDataSource, PeriodSettingDelegate{
     
     @IBOutlet var totalEnergy: UILabel!
     @IBOutlet var graphViewContainer: UIView!
     @IBOutlet var xAxisLabel: UILabel!
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var historyDeviceTableview: UITableView!
     
     var graphView: ScrollableGraphView!
     var fromDate: Date!
@@ -26,8 +27,11 @@ class RoomHistoryViewController: UIViewController, RoomPageControllerToPage, Scr
     var yAxis = [Double]()
     var xAxis = [Date]()
     
+    var devices = [Device]()
+    
     func reloadPage() {
         graphView.reload()
+        getData()
     }
     
     override func viewDidLoad() {
@@ -46,6 +50,7 @@ class RoomHistoryViewController: UIViewController, RoomPageControllerToPage, Scr
         toDate = Calendar.current.date(byAdding: components, to: fromDate!)
         
         getData(from: fromDate, to: toDate)
+        getData()
         
     }
     
@@ -289,5 +294,83 @@ class RoomHistoryViewController: UIViewController, RoomPageControllerToPage, Scr
     // MARK: Actions
     @IBAction func periodButtonPressed(_ sender: Any) {
         periodSettingScreen()
+    }
+    
+    //Tableview
+    
+    func getData() {
+        
+        if Helper.isConnectedToInternet() {
+            let headers: HTTPHeaders = [
+                "Authorization": "Bearer " + Helper.getStoredTokenString()!,
+                "Accept": "application/json"
+            ]
+            
+            Alamofire.request("\(Constants.Urls.api)/house/\(Helper.getStoredHouseId())/room/\(roomId!)/devices", headers: headers).responseJSON { response in
+                switch response.result {
+                case .success:
+                    print("Device info retrieved")
+                    do {
+                        self.devices = try JSONDecoder().decode([Device].self, from: response.data!)
+                        self.devices.sort(by: { $0.categoryName > $1.categoryName })
+                        self.historyDeviceTableview.reloadData()
+                    }catch {
+                        print("Parse error")
+                        
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+        else {
+            Helper.showAlertOneButton(viewController: self, title: "Geen netwerkverbinding", message: "Controleer of uw apparaat verbonden is met het internet", buttonTitle: "OK")
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return devices.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "historyDeviceCell", for: indexPath) as! DevicesTableViewCell
+        
+        // Get the device
+        let device: Device
+        device = devices[indexPath.row]
+        
+        // Set the values
+        cell.deviceName.text = device.name
+        
+        if device.energyUsage.usage != nil {
+            cell.deviceUsage.text = String(device.energyUsage.usage!) + " W"
+        }
+        
+        // Set the image according to the given iconName
+        switch device.categoryName {
+        case Constants.deviceCategories.multiSensor:
+            cell.deviceImage.image = UIImage(named: "multisensor")?.withRenderingMode(.alwaysTemplate)
+            break
+        case Constants.deviceCategories.light:
+            cell.deviceImage.image = UIImage(named: "lightbulb")?.withRenderingMode(.alwaysTemplate)
+            break
+        case Constants.deviceCategories.socket:
+            cell.deviceImage.image = UIImage(named: "powerplug")?.withRenderingMode(.alwaysTemplate)
+            break
+        case Constants.deviceCategories.doorSensor:
+            cell.deviceImage.image = UIImage(named: "movement")?.withRenderingMode(.alwaysTemplate)
+            break
+        default: break
+        }
+        
+        cell.preservesSuperviewLayoutMargins = false
+        cell.separatorInset = UIEdgeInsets.zero
+        cell.layoutMargins = UIEdgeInsets.zero
+        
+        //Set the icon tintcolor
+        cell.deviceImage.tintColor = UIColor(hexString: "#5ED0A8")
+        
+        return cell
     }
 }
