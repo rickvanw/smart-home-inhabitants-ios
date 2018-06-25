415accbdea5933d971e03ad80b48297d198d5e5a
Rick van Weersel <rickvanweersel@hotmail.com>
2018-06-25 22:19:42 +0200

    Added category filter for energy + some fixes
    Fixed large name in list causing pushed away energyUsage
    
    Fixed showing categories before list loaded

--- ElectriDash/ElectriDash/Rooms/RoomDevicesViewController.swift
+++ ElectriDash/ElectriDash/Rooms/RoomDevicesViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Alamofire
 
-class RoomDevicesViewController: UIViewController,UITableViewDataSource, UITableViewDelegate, RoomPageControllerToPage, TableViewCategory {
+class RoomDevicesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, RoomPageControllerToPage, TableViewCategory {
     
     var roomId: Int?
     
@@ -39,7 +39,7 @@ class RoomDevicesViewController: UIViewController,UITableViewDataSource, UITable
         
         deviceTableView.tableFooterView = UIView()
         getData()
-
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -48,7 +48,7 @@ class RoomDevicesViewController: UIViewController,UITableViewDataSource, UITable
     }
     
     func getData() {
-    
+        
         if Helper.isConnectedToInternet() {
             let headers: HTTPHeaders = [
                 "Authorization": "Bearer " + Helper.getStoredTokenString()!,
@@ -63,7 +63,7 @@ class RoomDevicesViewController: UIViewController,UITableViewDataSource, UITable
                         self.devices = try JSONDecoder().decode([Device].self, from: response.data!)
                         self.devices.sort(by: { $0.categoryName > $1.categoryName })
                         self.allDevices = self.devices
-
+                        
                         self.deviceTableView.reloadData()
                     }catch {
                         print("Parse error")
@@ -81,16 +81,11 @@ class RoomDevicesViewController: UIViewController,UITableViewDataSource, UITable
     
     // MARK: TableViewCategory
     func setCategory() {
-        
-        showCategories()
-    }
 
-
-    func showCategories() {
         let alert = UIAlertController(title: "Selecteer een categorie", message: nil, preferredStyle: .actionSheet)
         
         alert.view.tintColor = Constants.AppColors.loginGreen
-
+        
         let sortedCategories = categories.sorted {$0.value < $1.value}
         
         for category in sortedCategories{
@@ -101,7 +96,7 @@ class RoomDevicesViewController: UIViewController,UITableViewDataSource, UITable
                 if self.currentCategory == "all"{
                     self.devices = self.allDevices
                 }else{
-                self.devices = self.allDevices.filter { $0.categoryName == self.currentCategory }
+                    self.devices = self.allDevices.filter { $0.categoryName == self.currentCategory }
                 }
                 
                 self.deviceTableView.reloadData()
@@ -120,14 +115,18 @@ class RoomDevicesViewController: UIViewController,UITableViewDataSource, UITable
             popoverController.sourceView = button
             popoverController.sourceRect = CGRect(x: button.bounds.midX, y: button.bounds.maxY, width: 0, height: 0)
             popoverController.permittedArrowDirections = .up
-
+            
         }
         
         self.present(alert, animated: true, completion: nil)
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return devices.count + 1
+        if devices.count > 0{
+            return devices.count + 1
+        }else{
+            return 0
+        }
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
@@ -140,45 +139,45 @@ class RoomDevicesViewController: UIViewController,UITableViewDataSource, UITable
             return cell
             
         }else{
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "deviceCell", for: indexPath) as! DevicesTableViewCell
-        
-        // Get the device
-        let device: Device
-        device = devices[indexPath.row - 1]
-        
-        // Set the values
-        cell.deviceName.text = device.name
-        
-        if device.energyUsage.usage != nil {
-            cell.deviceUsage.text = String(Helper.getCurrencyOrW(energyUsage: device.energyUsage) + " " + Helper.getCurrencyOrWattName())
-        }
-        
-        // Set the image according to the given iconName
-        switch device.categoryName {
-        case Constants.deviceCategories.multiSensor:
-            cell.deviceImage.image = UIImage(named: "multisensor")?.withRenderingMode(.alwaysTemplate)
-            break
-        case Constants.deviceCategories.light:
-            cell.deviceImage.image = UIImage(named: "lightbulb")?.withRenderingMode(.alwaysTemplate)
-            break
-        case Constants.deviceCategories.socket:
-            cell.deviceImage.image = UIImage(named: "powerplug")?.withRenderingMode(.alwaysTemplate)
-            break
-        case Constants.deviceCategories.doorSensor:
-            cell.deviceImage.image = UIImage(named: "movement")?.withRenderingMode(.alwaysTemplate)
-            break
-        default: break
-        }
-        
-        cell.preservesSuperviewLayoutMargins = false
-        cell.separatorInset = UIEdgeInsets.zero
-        cell.layoutMargins = UIEdgeInsets.zero
-        
-        //Set the icon tintcolor
-        cell.deviceImage.tintColor = UIColor(hexString: "#5ED0A8")
-
-        return cell
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "deviceCell", for: indexPath) as! DevicesTableViewCell
+            
+            // Get the device
+            let device: Device
+            device = devices[indexPath.row - 1]
+            
+            // Set the values
+            cell.deviceName.text = device.name
+            
+            if device.energyUsage.usage != nil {
+                cell.deviceUsage.text = String(Helper.getCurrencyOrW(energyUsage: device.energyUsage) + " " + Helper.getCurrencyOrWattName())
+            }
+            
+            // Set the image according to the given iconName
+            switch device.categoryName {
+            case Constants.deviceCategories.multiSensor:
+                cell.deviceImage.image = UIImage(named: "multisensor")?.withRenderingMode(.alwaysTemplate)
+                break
+            case Constants.deviceCategories.light:
+                cell.deviceImage.image = UIImage(named: "lightbulb")?.withRenderingMode(.alwaysTemplate)
+                break
+            case Constants.deviceCategories.socket:
+                cell.deviceImage.image = UIImage(named: "powerplug")?.withRenderingMode(.alwaysTemplate)
+                break
+            case Constants.deviceCategories.doorSensor:
+                cell.deviceImage.image = UIImage(named: "movement")?.withRenderingMode(.alwaysTemplate)
+                break
+            default: break
+            }
+            
+            cell.preservesSuperviewLayoutMargins = false
+            cell.separatorInset = UIEdgeInsets.zero
+            cell.layoutMargins = UIEdgeInsets.zero
+            
+            //Set the icon tintcolor
+            cell.deviceImage.tintColor = UIColor(hexString: "#5ED0A8")
+            
+            return cell
         }
     }
 }
